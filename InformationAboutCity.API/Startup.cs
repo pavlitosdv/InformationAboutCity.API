27c57502252b370305735c75f84d579dbbec9b81
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-09-08 20:20:51 +0300

    Added support to output results in XML format (AddMvcOptions) and the option (currently is commented out) of have the JSON results started with the first letter in Upper case (AddJsonOptions)

--- InformationAboutCity.API/Startup.cs
+++ InformationAboutCity.API/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace InformationAboutCity.API
 {
@@ -15,6 +17,21 @@ namespace InformationAboutCity.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc()
+                .AddMvcOptions(o =>
+                {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                });
+            //the code below is useful when we want the JSON result to have the properties started with the first letter to be UPPER CASE
+            //.AddJsonOptions(o =>
+            //{
+            //    if (o.SerializerSettings.ContractResolver != null)
+            //    {
+            //        var castedResolver = o.SerializerSettings.ContractResolver
+            //                               as DefaultContractResolver;
+            //        castedResolver.NamingStrategy = null;
+            //    }
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
