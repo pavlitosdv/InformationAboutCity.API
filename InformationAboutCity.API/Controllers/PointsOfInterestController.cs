27793eed4414fcdbfbd4e5727ead96cc06215d38
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-09-08 20:05:50 +0300

    Added Points of Interest Controller and Point of interest DTO class
    Modified CityDTO class and CitiesMockData class

---
+++ InformationAboutCity.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InformationAboutCity.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesMockData.Current.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // find city
+            var cityToReturn = CitiesMockData.Current.Cities
+                .FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
--- InformationAboutCity.API/Models/CityDto.cs
+++ InformationAboutCity.API/Models/CityDto.cs
@@ -10,5 +10,18 @@ namespace InformationAboutCity.API.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        // we initialize it as a best practise in order to avoid null reference excemptions
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+         = new List<PointOfInterestDto>();
+
     }
 }
