ea52df334bd3fd0f1d0b4d5c77f6ff970b89728d
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-09-08 19:36:30 +0300

    Added Cities Controller Added Model DTO Added Mock DAta

---
+++ InformationAboutCity.API/CitiesMockData.cs
@@ -0,0 +1,41 @@
+﻿using InformationAboutCity.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InformationAboutCity.API
+{
+    public class CitiesMockData
+    {
+        public static CitiesMockData Current { get; } = new CitiesMockData();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesMockData()
+        {
+            // init dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                     Id = 1,
+                     Name = "New York City",
+                     Description = "The one with that big park."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really finished."
+                },
+                new CityDto()
+                {
+                    Id= 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower."
+                }
+            };
+        }
+    }
+}
---
+++ InformationAboutCity.API/Controllers/CitiesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InformationAboutCity.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesMockData.Current.Cities);
+
+            #region mock data (obsolete)
+            //return new JsonResult(new List<object>()
+            //{
+            //    new { id=1, Name= "City of New York" },
+            //    new { id=2, Name= "Belgium City" }
+            //});
+            #endregion
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // find city
+            var cityToReturn = CitiesMockData.Current.Cities
+                .FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
