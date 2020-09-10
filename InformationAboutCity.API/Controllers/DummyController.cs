9ab8acfe1952316ab979030cf10e72182583ab38
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-09-10 22:04:23 +0300

    -DBContext added
    -DbContext service added to the startup.cs class and configured
    
    -the connection string added to the appsettings
    
    -Dummy controller added which can be used to create the database when its action result method is called via the constructor.

---
+++ InformationAboutCity.API/Controllers/DummyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InformationAboutCity.API.DBContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InformationAboutCity.API.Controllers
+{
+                                  //this controller is used to create the database. by calling the TestDatabase the \\
+                                  // constructor initializes the CityInfoContext and the database is generated \\
+
+    public class DummyController : ControllerBase
+    {
+        private readonly CityInfoContext _ctx;
+
+        public DummyController(CityInfoContext ctx)
+        {
+            _ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        [HttpGet]
+        public IActionResult TestDatabase()
+        {
+            return Ok();
+        }
+    }
+}
--- InformationAboutCity.API/Startup.cs
+++ InformationAboutCity.API/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InformationAboutCity.API.DBContext;
 using InformationAboutCity.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -14,6 +17,13 @@ namespace InformationAboutCity.API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -41,6 +51,12 @@ namespace InformationAboutCity.API
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
+
+            var connectionString = _configuration["connectionStrings:cityInfoDBConnectionString"];
+            services.AddDbContext<CityInfoContext>(o =>
+            {
+                o.UseSqlServer(connectionString);
+            });
 
         }
 
